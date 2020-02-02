52dabdec1007733e093ebead53af4b8c60e5173c
looping-bell-1500 <gabrielalcantara7@yahoo.com>
2020-02-02 05:30:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
- if 
- else board[3] == "X" || "O"
+ if board[3] == "X" || "O"
    true
+ else board[3] == " " || "" || nil
+   false
   end
 end
